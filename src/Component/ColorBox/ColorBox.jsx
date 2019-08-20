563190bc0ba87af836b3725b53cfb94ef2271ffd
Faysal Ahmed <47469289+faysal146@users.noreply.github.com>
2019-08-21 01:23:34 +0600

    add copy animaiton when color is copy

--- src/Component/ColorBox/ColorBox.jsx
+++ src/Component/ColorBox/ColorBox.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ColorBox.scss';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-const ColorBox = ({ name, background }) => {
-	return (
-		<CopyToClipboard text={background}>
-			<div
-				className="color-box"
-				style={{
-					background
-				}}
-			>
-				<div className="color-box__content">
-					<span className="color-box__name">{name}</span>
-					<button className="color-box__button">copy</button>
-					<span className="color-box__more-btn">More</span>
+class ColorBox extends Component {
+	state = {
+		isCopied: false
+	};
+	handleClick = () => {
+		this.setState({isCopied: true}, () => {
+			setTimeout(() => {
+				this.setState({isCopied:false})
+			}, 1500) 
+		})
+	}
+	render() {
+		const { name, background } = this.props;
+		const { isCopied } = this.state;
+		return (
+			<CopyToClipboard text={background} onCopy={this.handleClick}>
+				<div className="color-box" style={{background}}>
+					<div className={`color-box__overlay ${isCopied && 'color-box__overlay_show'}`} style={{ background  }} />
+
+					<div className={`color-box__copied-mas ${this.state.isCopied && 'color-box__copied-mas_show'}`}>
+						<h1>Copied !</h1>
+						<p>{background}</p>
+					</div>
+
+					<div className="color-box__content">
+						<span className="color-box__name">{name}</span>
+						<button className="color-box__button">copy</button>
+						<span className="color-box__more-btn">More</span>
+					</div>
 				</div>
-			</div>
-		</CopyToClipboard>
-	);
-};
+			</CopyToClipboard>
+		);
+	}
+}
 export default ColorBox;
