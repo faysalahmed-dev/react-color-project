8c1a8cfda21da06a6060e808df3a92253249601e
Faysal Ahmed <47469289+faysal146@users.noreply.github.com>
2019-08-20 22:03:54 +0600

    add copy future in colorbox

--- src/Component/ColorBox/ColorBox.jsx
+++ src/Component/ColorBox/ColorBox.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import './ColorBox.scss';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const ColorBox = ({ name, background }) => {
 	return (
-		<div
-			className="color-box"
-			style={{
-				background
-			}}
-		>
-
-			<div className="color-box__content">
-				<span className="color-box__name">{name}</span>
-				<button className="color-box__button">copy</button>
-				<span className="color-box__more-btn">More</span>
+		<CopyToClipboard text={background}>
+			<div
+				className="color-box"
+				style={{
+					background
+				}}
+			>
+				<div className="color-box__content">
+					<span className="color-box__name">{name}</span>
+					<button className="color-box__button">copy</button>
+					<span className="color-box__more-btn">More</span>
+				</div>
 			</div>
-		</div>
+		</CopyToClipboard>
 	);
 };
 export default ColorBox;
