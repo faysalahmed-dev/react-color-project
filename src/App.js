3cc367e304dd12a98024d9e02cdda41b56efd53f
Faysal Ahmed <47469289+faysal146@users.noreply.github.com>
2019-08-26 21:25:59 +0600

    add color shade slider

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import ColorPalette from './Component/ColorPalette/ColorPalette';
 import SeedColors from './SeedColors';
 import ColorShadGenerator from './HelperColors/HelperColors'
 
+
+
 const App = () => {
-	console.log(ColorShadGenerator(SeedColors[3]))
 	return (
 		<div className="App">
-			<ColorPalette {...SeedColors[6]} />
+			<ColorPalette palette={ColorShadGenerator(SeedColors[3])} />
 		</div>
 	);
 };
--- src/Component/ColorPalette/ColorPalette.jsx
+++ src/Component/ColorPalette/ColorPalette.jsx
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
+import Slider from 'rc-slider';
+
 import ColorBox from '../ColorBox/ColorBox';
 
+import 'rc-slider/assets/index.css';
 import './ColorPalette.scss';
 
 class ColorPalette extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			colorShade: 800
+		};
+	}
+	changeColorShade = (colorShade) => {
+		this.setState({ colorShade });
+	};
 	render() {
+		const { colors } = this.props.palette;
+		const { colorShade } = this.state;
 		return (
 			<div className="palette">
-				{/* nav bar is here */}
-
+				<Slider
+					defaultValue={colorShade}
+					min={100}
+					max={900}
+					step={100}
+					onAfterChange={this.changeColorShade}
+				/>
 				<div className="palette__colors">
-					{this.props.colors.map(({ name, color }, idx) => <ColorBox name={name} background={color} key={idx} />)}
+					{colors[colorShade].map(({ name, hex }, idx) => (
+						<ColorBox name={name} background={hex} key={idx} />
+					))}
 				</div>
 
 				{/* footer here */}
